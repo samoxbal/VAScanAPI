712387994b5e2bbef3057416d66c37675f36bd9e
Dmitry <7805382@mail.ru>
2017-06-13 20:57:16 +0300

    Initial commit

---
+++ src/main/java/ru/vascan/api/ApiApplication.java
@@ -0,0 +1,12 @@
+package ru.vascan.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiApplication.class, args);
+	}
+}
