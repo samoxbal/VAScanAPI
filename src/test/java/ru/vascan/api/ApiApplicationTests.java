db036c97bf8b60b9085f4a35e5d212188d15d2ff
Dmitry <7805382@mail.ru>
2017-07-07 22:34:30 +0300

    Add billet for unit tests

--- src/test/java/ru/vascan/api/ApiApplicationTests.java
+++ src/test/java/ru/vascan/api/ApiApplicationTests.java
@@ -1,16 +1,37 @@
 package ru.vascan.api;
 
 import org.junit.Test;
+import org.junit.Before;
 import org.junit.runner.RunWith;
+import org.springframework.http.MediaType;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
-@RunWith(SpringRunner.class)
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = ApiApplication.class)
 @SpringBootTest
 public class ApiApplicationTests {
 
-	@Test
-	public void contextLoads() {
+	private MockMvc mockMvc;
+
+	@Autowired
+    private WebApplicationContext wac;
+
+	@Before
+	public void init() {
+        this.mockMvc = MockMvcBuilders
+				.webAppContextSetup(wac)
+				.build();
 	}
 
+	@Test
+	public void testExperimentsQuery() {
+
+	}
 }
